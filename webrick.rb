3e04245f20c4ac3fc4993092937675f9c0f36d37
yanger <kanayaofficial1129@gmail.com>
2024-04-24 22:43:13 +0900

    Add form

--- webrick.rb
+++ webrick.rb
@@ -15,6 +15,30 @@ server.mount_proc("/time") do |req, res|
   res.body = body
 end
 
+server.mount_proc("/form_get") do |req, res|
+  req.query
+  body = "<html><head><meta charset=\"utf-8\"/></head><body>"
+  body += "<p>こんにちは、#{ req.query }</p>"
+  body += "<p>あなたの名前は#{ req.query['username'] }ですね。</p>"
+  body += "<p>あなたの年齢は#{ req.query['age'] }ですね。</p>"
+  body += "</body></html>"
+  res.status = 200
+  res['Content-Type'] = 'text/html'
+  res.body = body
+end
+
+server.mount_proc("/form_post") do |req, res|
+  req.query
+  body = "<html><head><meta charset=\"utf-8\"/></head><body>"
+  body += "<p>こんにちは、#{ req.query }</p>"
+  body += "<p>あなたの名前は#{ req.query['username'] }ですね。</p>"
+  body += "<p>あなたの年齢は#{ req.query['age'] }ですね。</p>"
+  body += "</body></html>"
+  res.status = 200
+  res['Content-Type'] = 'text/html'
+  res.body = body
+end
+
 trap(:INT){
     server.shutdown
 }
