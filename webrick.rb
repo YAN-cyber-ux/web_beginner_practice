b798fc54bbbf729716931fcf9746aa1899a8427c
yanger <kanayaofficial1129@gmail.com>
2024-04-25 12:36:33 +0900

    Add erb

--- webrick.rb
+++ webrick.rb
@@ -1,5 +1,6 @@
  # webrick.rb
 require 'webrick'
+require 'erb'
 
 server = WEBrick::HTTPServer.new({ 
   :DocumentRoot => './',
@@ -38,6 +39,16 @@ server.mount_proc("/form_post") do |req, res|
   res['Content-Type'] = 'text/html'
   res.body = body
 end
+
+WEBrick::HTTPServlet::FileHandler.add_handler("erb", WEBrick::HTTPServlet::ERBHandler)
+server.config[:MimeTypes]["erb"] = "text/html"
+
+server.mount_proc("/hello") do |req, res|
+  template = ERB.new( File.read('hello.erb') )
+  time = Time.new
+  res.body << template.result( binding )
+end
+
 
 trap(:INT){
     server.shutdown
